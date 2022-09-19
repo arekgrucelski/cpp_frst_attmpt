98007d4e2b27e8b69a15bacd230238f57ee9b06c
arekgrucelski <arek@agicortex.com>
2022-09-19 10:51:15 +0200

    'start.sh' script modification start.sh was based on data from my system ...some independence is given

--- start.sh
+++ start.sh
@@ -10,14 +10,19 @@
 # \todo the output is hard to understand
 ##*/
 
-export LD_LIBRARY_PATH='/home/agrucelski/.julia/artifacts/4fcd159fccd2f12b8c8c3da884709dc1de7a30ae/lib/:/home/agrucelski/Dokumenty/git_mw/cpp_frst_attmpt/:/opt/julia-1.6.0/lib'
+cxxwrap_drnm=$(dirname `find ~/.julia/ -name "libcxxwrap_julia.so"`| head -n 1)
+tst_drnm=$(pwd)
 
+LD_LIBRARY_PATH=$cxxwrap_drnm
+LD_LIBRARY_PATH+=":"$tst_drnm
+export LD_LIBRARY_PATH+=':/opt/julia-1.6.0/lib'
+##
 g++ -std=c++1z -I/opt/julia-1.6.0/include/julia/ -DCxxJULIA -c -Wall -Werror -fpic src/tst.cpp
 g++ -shared -o libtst.so tst.o 
 ##`pkg-config --cflags --libs julia`
 ##`pkg-config --cflags --libs opencv`
 
-g++ -std=c++1z -L/opt/julia-1.6.0/lib/ -L/home/agrucelski/.julia/artifacts/4fcd159fccd2f12b8c8c3da884709dc1de7a30ae/lib/ -L/home/agrucelski/Dokumenty/git_mw/cpp_frst_attmpt/ -I/opt/julia-1.6.0/include/julia/ -DCxxJULIA -Wall -o test src/main.cpp -ltst -ljulia -lcxxwrap_julia
+g++ -std=c++1z -L/opt/julia-1.6.0/lib/ -L$cxxwrap_drnm -L$tst_drnm -I/opt/julia-1.6.0/include/julia/ -DCxxJULIA -Wall -o test src/main.cpp -ltst -ljulia -lcxxwrap_julia
 ##`pkg-config --cflags --libs cxxwrap_julia`
 ##`pkg-config --cflags --libs opencv`
 
