19b35c8d6be9216accdeb28416ff38baa27845ab
arekgrucelski <arek@agicortex.com>
2022-09-19 11:54:28 +0200

    Some further improvement of start.sh

--- start.sh
+++ start.sh
@@ -12,20 +12,26 @@
 
 cxxwrap_drnm=$(dirname `find ~/.julia/ -name "libcxxwrap_julia.so"`| head -n 1)
 tst_drnm=$(pwd)
+jlhdr_drnm=$(dirname `locate julia.h |grep include |tail -n 1`)
+jllib_drnm=$(dirname `locate -b '\libjulia.so.1' |tail -n 1`)
 
 LD_LIBRARY_PATH=$cxxwrap_drnm
 LD_LIBRARY_PATH+=":"$tst_drnm
-export LD_LIBRARY_PATH+=':/opt/julia-1.6.0/lib'
+LD_LIBRARY_PATH+=":"$jllib_drnm
+export LD_LIBRARY_PATH
+##echo $LD_LIBRARY_PATH
 ##
-g++ -std=c++1z -I/opt/julia-1.6.0/include/julia/ -DCxxJULIA -c -Wall -Werror -fpic src/tst.cpp
+g++ -std=c++1z -I$jlhdr_drnm -DCxxJULIA -c -Wall -Werror -fpic src/tst.cpp
 g++ -shared -o libtst.so tst.o 
 ##`pkg-config --cflags --libs julia`
 ##`pkg-config --cflags --libs opencv`
 
-g++ -std=c++1z -L/opt/julia-1.6.0/lib/ -L$cxxwrap_drnm -L$tst_drnm -I/opt/julia-1.6.0/include/julia/ -DCxxJULIA -Wall -o test src/main.cpp -ltst -ljulia -lcxxwrap_julia
+g++ -std$=c++1z -L/opt/julia-1.6.0/lib/ -L$cxxwrap_drnm -L$tst_drnm -I/opt/julia-1.6.0/include/julia/ -DCxxJULIA -Wall -o test src/main.cpp -ltst -ljulia -lcxxwrap_julia
 ##`pkg-config --cflags --libs cxxwrap_julia`
 ##`pkg-config --cflags --libs opencv`
 
+#echo $jlhdr_drnm
+#echo $jllib_drnm
 echo "C++ "
 ./test
 
